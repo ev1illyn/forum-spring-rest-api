638cbee5e184314eab20f74e5d1b2f5080056446
evillyn <evillyn.oliveira@estagiario.ufca.edu.br>
2020-10-27 20:29:53 -0300

    mapping models and add spring-data-jpa dependency

--- src/main/java/br/com/alura/forum/modelo/Curso.java
+++ src/main/java/br/com/alura/forum/modelo/Curso.java
@@ -1,7 +1,16 @@
 package br.com.alura.forum.modelo;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Entity;
+
+@Entity
 public class Curso {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String categoria;
