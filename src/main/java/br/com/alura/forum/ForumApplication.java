4cd56a10f1f0ba4340c1197fdd34e3d2d08d1691
ev1illyn <evillyndsoliveiras@gmail.com>
2020-12-21 11:42:48 -0300

    setting app build do .jar

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,6 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -15,15 +13,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableJpaRepositories(basePackages = {"br.com.alura.forum"})
 @EnableCaching
 @EnableSwagger2
-public class ForumApplication extends SpringBootServletInitializer{
+public class ForumApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
 	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ForumApplication.class);
-	}
-
 }
